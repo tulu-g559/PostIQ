fc1ade60f220ee13ffb82be4951d62406c6ea530
Arnab Ghosh <garnab559@gmail.com>
2026-02-21 16:01:00 +0000

    gg

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-4895938370-198f8",
-  "appId": "1:464793737914:web:00d2d5d1daf912de4a0083",
-  "apiKey": "AIzaSyASLYxf8t9c4wTBftgbtfkgZGjcpzNvEmM",
-  "authDomain": "studio-4895938370-198f8.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "464793737914"
-};
